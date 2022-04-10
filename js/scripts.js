d96cc79e12721b4d4c7f438dc50ee8a50fd38e52
Alex Habibi <AwatHabibi@icloud.com>
2022-04-10 00:09:48 -0400

    Add functionality with js scripts

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function()  {
+  $("form#question5").submit(function(event)  {
+    const question5 = $("select#question5").val();
+
+    if (question5 === "make websites" )  {
+      $("#result").show("JavaScript");
+    } else if (question5 === "make apps") {
+      $("#result").show("Swift");
+
+      event.preventDefault();
+    }
+  })
+})
